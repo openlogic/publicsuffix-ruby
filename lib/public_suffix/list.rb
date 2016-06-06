7b9da4057d8d363ad361ec3a92ad00908f4be94c
Aaron Botnick <aaron.botnick@roguewave.com>
2016-06-06 08:23:14 -0600

    Add require for tempdir so that accessing a temp directory for the definitions list is possible

--- lib/public_suffix/list.rb
+++ lib/public_suffix/list.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'tempdir'
 #
 # Public Suffix
 #
--- lib/public_suffix/version.rb
+++ lib/public_suffix/version.rb
@@ -12,7 +12,7 @@ module PublicSuffix
     MAJOR = 1
     MINOR = 5
     PATCH = 1
-    BUILD = 5
+    BUILD = 6
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
   end
